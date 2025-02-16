293c4d1955e21b2886116b596a3374f1f76e90e7
Maria <zaush@bk.ru>
2025-02-16 14:35:20 +0300

    added swagger

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -8,6 +8,11 @@ import (
 
 // health необходим для проверки работоспособности сервера.
 // Всегда отвечает 200 ОК
+// health godoc
+//	@Summary		Проверить состояние сервера и соединения
+//	@Description	Проверить состояние сервера и соединения
+//	@Success		200
+//	@Router			/health [get]
 func (s *server) health(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
